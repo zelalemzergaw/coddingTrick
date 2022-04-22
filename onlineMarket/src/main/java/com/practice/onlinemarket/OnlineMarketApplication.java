9ff5f4079ef70293c8aa98ba1d05667f7867c990
zelalemzergaw <zelalem443@gmail.com>
2022-04-22 00:16:27 -0500

    My first project commit

---
+++ onlineMarket/src/main/java/com/practice/onlinemarket/OnlineMarketApplication.java
@@ -0,0 +1,14 @@
+package com.practice.onlinemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class OnlineMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMarketApplication.class, args);
+    }
+
+}
